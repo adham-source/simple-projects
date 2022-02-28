6c2ec0bd7193171f39c03b3d24a4d955e2d47de2
adham-source <adham.ahmad1988as@gmail.com>
2022-02-28 14:58:21 +0200

    Add static project

---
+++ dr_samuel_boshra/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.listen(
+  process.env.PORT || 3000,
+  console.info(`Server runing port ${process.env.PORT || 3000}`)
+)
